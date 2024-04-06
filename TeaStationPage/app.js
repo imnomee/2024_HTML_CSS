604cdca28b9bde22215ec9eb0830caabf40278b2
Noman Rafiq <heyimnomee@gmail.com>
2024-04-07 01:17:45 +0500

    animation navbar

---
+++ TeaStationPage/app.js
@@ -0,0 +1,14 @@
+const dat = (document.getElementById('date').innerHTML =
+    new Date().getFullYear());
+
+const navBtn = document.getElementById('nav-btn');
+
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
+
+navBtn.addEventListener('click', () => {
+    navbar.classList.add('showNav');
+});
+navClose.addEventListener('click', () => {
+    navbar.classList.remove('showNav');
+});
